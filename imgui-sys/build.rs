7f020e5dd96ac9c3cbad16361360a5a55c5b67cb
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2022-09-30 12:11:01 +0900

    Ensure that imgui sources are available in OUT_DIR
    Exposing the location of third_party location to be the build location
    does not work when the build location is temporary. Instead, use
    OUT_DIR.
    
    Also now that we include the sources in the output explicitly under
    `include`, I put the library under `lib` which is customary.

--- imgui-sys/build.rs
+++ imgui-sys/build.rs
@@ -23,21 +23,58 @@ fn main() -> std::io::Result<()> {
     let docking_enabled = std::env::var_os("CARGO_FEATURE_DOCKING").is_some();
     let wasm_enabled = std::env::var_os("CARGO_FEATURE_WASM").is_some();
 
-    let cimgui_dir = if docking_enabled {
-        manifest_dir.join("third-party/imgui-docking")
-    } else {
-        manifest_dir.join("third-party/imgui-master")
-    };
+    // OUT_DIR is always set: either by cargo or whatever build system is
+    // mimicking it.
+    //
+    // It's also the default for `cc::Build` though we set it anyway to ensure
+    // no divergence in the future.
+    let dst = std::path::PathBuf::from(std::env::var_os("OUT_DIR").unwrap());
 
-    // For projects like implot-rs we expose the path to our cimgui
-    // files, via `DEP_IMGUI_THIRD_PARTY` env-var, so they can build
-    // against the same thing
-    println!("cargo:THIRD_PARTY={}", cimgui_dir.display());
+    // A directory in OUT_DIR where we'll put the imgui sources. Crucially, this
+    // means that this can point _outside_ of the build directory! Some systems
+    // build in a sanbox in a transient build directory but set OUT_DIR to
+    // somewhere permanent/immovable. If we don't do this, the below
+    // cargo:THIRD_PARTY will point at the possibly transient build directory
+    // and will cause anyone depending on DEP_IMGUI_THIRD_PARTY to fail.
+    //
+    // You can see other -sys crates do this as well, such as libz-sys.
+    let dst_sources = dst.join("include");
+    // Poor-man's recursive copy.
+    fn copy_dir_all(src: &std::path::Path, dst: &std::path::Path) -> std::io::Result<()> {
+        std::fs::create_dir_all(&dst)?;
+        for entry in std::fs::read_dir(src)? {
+            let entry = entry?;
+            let ty = entry.file_type()?;
+            if ty.is_dir() {
+                copy_dir_all(&entry.path(), &dst.join(entry.file_name()))?;
+            } else {
+                std::fs::copy(entry.path(), &dst.join(entry.file_name()))?;
+            }
+        }
+        Ok(())
+    }
+    copy_dir_all(
+        &if docking_enabled {
+            manifest_dir.join("third-party/imgui-docking")
+        } else {
+            manifest_dir.join("third-party/imgui-master")
+        },
+        &dst_sources,
+    )
+    .unwrap();
+
+    // We can now expose the path of the imgui files to other projects like
+    // implot-rs via `DEP_IMGUI_THIRD_PARTY` env-var, so they can build against
+    // the same thing.
+    println!("cargo:THIRD_PARTY={}", dst_sources.display());
 
     // If we aren't building WASM output, bunch of extra stuff to do
     if !wasm_enabled {
         // C++ compiler
         let mut build = cc::Build::new();
+        // Put the library in lib, as now we're also including sources in
+        // OUT_DIR.
+        build.out_dir(&dst.join("lib"));
         build.cpp(true);
 
         // Set defines for compiler
@@ -57,9 +94,9 @@ fn main() -> std::io::Result<()> {
             build.define("IMGUI_ENABLE_FREETYPE", None);
             println!("cargo:DEFINE_IMGUI_ENABLE_FREETYPE=");
 
-            // imgui_freetype.cpp needs access to `#include "imgui.h"`.
-            // So we include something like '[...]/third-party/imgui-master/imgui/'
-            build.include(cimgui_dir.join("imgui"));
+            // imgui_freetype.cpp needs access to `#include "imgui.h"` so we
+            // include the directory that contains it.
+            build.include(dst_sources.join("imgui"));
         }
 
         // Which "all imgui" file to use
